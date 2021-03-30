544dc344b09ac3cc76361b186448504d3a8ed025
pfeeny84 <77941384+pfeeny84@users.noreply.github.com>
2021-03-29 17:58:46 -0700

    Finished lab 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+class Guitar:
+    def __init__(self, brand, model, description, year):
+        self.brand = brand
+        self.model = model
+        self.description = description
+        self.year = year
+
+guitars = [
+    Guitar('Gibson', 'Les Paul Custom', 'Super sick guitar', 1956),
+    Guitar('Fender', 'Telecaster', 'Twaaaaaaang', 1975),
+
+]
+
+def home(request):
+  return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def guitars_index(request):
+    return render(request, 'guitars/index.html', { 'guitars': guitars })
